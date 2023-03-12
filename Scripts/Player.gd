5cb5d9920bece80f6ba0d7c0472e834a275079e8
yosyo <yosyomiyori@gmail.com>
2023-03-12 16:53:34 +0100

    c'est completement crazy les amis
    Co-Authored-By: Shakoh <70974710+Shakohh@users.noreply.github.com>

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,9 +2,17 @@ extends CharacterBody3D
 
 const SPEED = 15.0
 const JUMP_VELOCITY = 20
+const JETPACK_VELOCITY = 10
+
+var jetpack_fuel = 7
+var jetpack_enable = false
 
 @onready var _spring_arm: SpringArm3D = $Node3D/SpringArm
 @onready var _mesh: MeshInstance3D = $%MeshInstance3D
+
+@onready var _uiFuelBar: ProgressBar = $UI/MarginContainer/ProgressBar
+
+@onready var _rc: RayCast3D = $RayCast3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -15,17 +23,29 @@ func _unhandled_input(event: InputEvent):
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	if is_on_floor():
+		jetpack_fuel = 7
+		jetpack_enable = false
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and !is_on_floor():
+		jetpack_enable = true
+
+	if Input.is_action_pressed("jump") and jetpack_enable and jetpack_fuel > 0:
+		velocity.y = JETPACK_VELOCITY
+		jetpack_fuel -= 0.1
+
+	_uiFuelBar.value = jetpack_fuel
 
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
 
 	direction = direction.rotated(Vector3.UP, _spring_arm.rotation.y)
-	
-	#print(direction.length())
-	
+
+		#print(direction.length())
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -37,6 +57,5 @@ func _physics_process(delta):
 
 func _process(delta):
 	_spring_arm.position = Vector3(position.x, position.y + 2, position.z)
-	if velocity != Vector3.ZERO:
-		_mesh.rotation.y = lerp_angle(_mesh.rotation.y, atan2(-velocity.x, -velocity.z), 20 * delta)
-		_mesh.rotation.y
+	if velocity.x != 0 or velocity.z != 0 :
+		_mesh.rotation.y = lerp_angle(_mesh.rotation.y, atan2(-velocity.x, -velocity.z), 13 * delta)
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -2,21 +2,38 @@ extends ColorRect
 
 @onready var animator: AnimationPlayer = $AnimationPlayer
 
-@onready var playButton: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ResumeButton
-@onready var optionButton: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/OptionButton
-@onready var quitButton: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
+@onready var playButton: Button = $CenterContainer/PauseMenu/MarginContainer/VBoxContainer/ResumeButton
+@onready var optionButton: Button = $CenterContainer/PauseMenu/MarginContainer/VBoxContainer/OptionButton
+@onready var quitButton: Button = $CenterContainer/PauseMenu/MarginContainer/VBoxContainer/QuitButton
+
+@onready var fullscreen: Button = $OptionCenterContainer/OptionMenu/MarginContainer/VBoxContainer/HBoxContainer/Fullscreen
+@onready var backButton: Button = $OptionCenterContainer/OptionMenu/MarginContainer/VBoxContainer/BackButton
 
 func _ready():
 	playButton.pressed.connect(unpause)
-	optionButton.pressed.connect(unpause)
+	optionButton.pressed.connect(optionOpen)
+	backButton.pressed.connect(optionClose)
 	quitButton.pressed.connect(get_tree().quit)
+
 
 func unpause():
 	animator.play("Unpause")
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func optionClose():
+	animator.play("closeOptions")
+
+func optionOpen():
+	animator.play("openOptions")
+
 func pause():
 	animator.play("Pause")
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_fullscreen_toggled(button_pressed):
+	if button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
